9428d6c8e95b486575ea70512bb3242db4ab4b55
Mondanzo <hamburg.karl.nehring@gmail.com>
2019-08-31 22:35:49 +0200

    EVEN COOLER NAVBAR! :art:

--- public/js/components/navigation.vue
+++ public/js/components/navigation.vue
@@ -3,10 +3,10 @@
   <div class="branding">
     Jugendhackt App
   </div>
-  <div class="nav" id="navigation">
+  <div class="nav">
     <router-link to="/"><img class="icon" src="assets/icons/calendar.svg" alt="Events"></router-link>
     <router-link to="/hackdash"><img class="icon" src="assets/icons/clipboard.svg" alt="Hackdash"></router-link>
-    <router-link to="/login"><object type="image/svg+xml" class="icon" data="assets/icons/key.svg" alt="Login"></object></router-link>
+    <router-link to="/login"><img class="icon" src="assets/icons/key.svg" alt="Login"></router-link>
   </div>
 </nav>
 </template>
