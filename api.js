204507f6b5da9c2925be7da0825f48e694b84fc3
marvinborner <develop@marvinborner.de>
2019-08-31 13:48:15 +0200

    Added project page

--- api.js
+++ api.js
@@ -10,9 +10,14 @@ const apiUrls = {
 	"Zulip": "/api/Zulip"
 }
 
-api.use("/github", async (_, res) => res.send(await github.get()));
-api.use("/hackdash/list", async (_, res) => res.send(await hackdash.listBoards()));
-api.use("/hackdash/board/:board", async (req, res) => res.send(await hackdash.getBoardInfo(req.params.board)));
+api.use("/github",
+	async (_, res) => res.send(await github.get()));
+api.use("/hackdash/list",
+	async (_, res) => res.send(await hackdash.listBoards()));
+api.use("/hackdash/board/:board",
+	async (req, res) => res.send(await hackdash.getBoardInfo(req.params.board)));
+api.use("/hackdash/project/:pid",
+	async (req, res) => res.send(await hackdash.getProjectInfo(req.params.pid)));
 api.use("*", (_, res) => res.send(apiUrls));
 
 module.exports = api;
--- data/hackdash.js
+++ data/hackdash.js
@@ -6,7 +6,12 @@ module.exports.listBoards = async () => {
 	return boards["dashboards"];
 }
 
-module.exports.getBoardInfo = async (board) => {
+module.exports.getBoardInfo = async board => {
 	const data = await request.start("https://hackdash.org/api/v2/dashboards/" + board);
+	return data;
+}
+
+module.exports.getProjectInfo = async project_id => {
+	const data = await request.start("https://hackdash.org/api/v2/projects/" + project_id);
 	return data;
 }
