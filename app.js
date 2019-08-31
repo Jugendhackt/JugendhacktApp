df46476427571249e93063a56f247f40e2a59cb9
marvinborner <develop@marvinborner.de>
2019-08-31 20:58:43 +0200

    Service worker

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ const PORT = process.env.PORT || 80;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use((_, res, next) => {
+	res.append("Service-Worker-Allowed", "/");
+	next();
+});
 app.use(session({
 	secret: 'theBestSecretKey', // TODO: Random key
   	resave: false,
