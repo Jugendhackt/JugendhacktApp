3da8e7a01d926b54c5f4bcdb48874b3119efccec
Mondanzo <hamburg.karl.nehring@gmail.com>
2019-08-31 21:23:09 +0200

    Changed Icon :art:

--- public/js/components/navigation.vue
+++ public/js/components/navigation.vue
@@ -6,6 +6,7 @@
   <div class="nav hide" id="navigation">
     <br>
     <router-link to="/"><img class="icon" src="assets/icons/calendar.svg" alt="Events"></router-link>
+    <router-link to="/hackdash"><img class="icon" src="assets/icons/clipboard.svg" alt="Hackdash"></router-link>
     <router-link to="/login"><img class="icon" src="assets/icons/key.svg" alt="Login"></router-link>
   </div>
 </nav>
