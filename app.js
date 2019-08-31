0e37356a02a5ba36f99ded44122e25e8068ea20c
marvinborner <develop@marvinborner.de>
2019-08-31 11:20:29 +0200

    Improved api

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const github = require("./data/github.js");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const api = express.Router();
+api.use("/github", (_, req) => github.get(data => req.send(data)));
+app.use("/api", api);
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
--- data/github.js
+++ data/github.js
@@ -1,16 +1,20 @@
 const https = require("https");
 
-module.exports.github = (callback) => {
-	let output;
+module.exports.get = (callback) => {
+	let output = "";
 	const req = https.request(
 		{
-			host: "api.github.com/orgs/JugendHackt/repos",
-			port: 443
+			host: "api.github.com",
+			port: 443,
+			headers: {
+				"User-Agent": "JugendHackt"
+			},
+			path: "/orgs/JugendHackt/repos"
 		}, res => {
 			res.on("data", chunk => output += chunk);
 			res.on("end", () => callback(JSON.parse(output)));
 		});
 
-	req.on("error", err => console.error(err));
+	req.on("error", err => callback(err));
 	req.end();
 }
