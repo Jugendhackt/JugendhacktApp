6962b8c03c39ff8385ab2eb3f3723940582b4321
Mondanzo <hamburg.karl.nehring@gmail.com>
2019-09-01 01:10:36 +0200

    Continued Work on Hackdash
    The boards are getting grabbed now but can't get loaded yet.

--- public/views/hackdash.vue
+++ public/views/hackdash.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <router-view></router-view>
+    <!--<board v-if="$route.params.hasOwnAttribute('board')" :board="$route.params.board"></board>-->
     <div class="board-navigation">
-      <router-link to="jhhh2019" class="board">JHHH2019</router-link>
+      <router-link v-for="board in boards" :to="'hackdash/' + board.domain" class="board">{{board.domain}}</router-link>
     </div>
   </div>
 </template>
 
 <script>
+
 module.exports = {
   data: function(){
     return {
-      boards: [],
+      boards: {},
       loading: false
     }
   },
@@ -20,36 +21,47 @@ module.exports = {
       this.loading = true;
       let xhr = new XMLHttpRequest();
       xhr.addEventListener("load", () => {
+        console.log(xhr.response);
         this.loading = false;
-        boards = xhr.response;
+        this.boards = xhr.response;
       });
       xhr.open("GET", "/api/hackdash");
       xhr.responseType = "json";
       xhr.send();
     }
+  },
+  beforeMount(){
+    this.fetch();
   }
 }
 </script>
 <style scoped>
 .board-navigation {
+  border: solid 1px #cecece;
   position: fixed;
-  font-size: .5em;
+  display: flex;
+  font-size: .75em;
   height: 64px;
   bottom: 66px;
   left: 0;
   right: 0;
-  overflow: scroll-y;
+  overflow: scroll;
   background: #fff;
   z-index: -1;
 }
 
 .board {
   display: flex;
-  border: solid 1px #cecece;
+  border-right: solid 1px #cecece;
   justify-content: center;
   align-items: center;
-  height: 64px;
-  width: 64px;
+  min-height: 64px;
+  min-width: 64px;
   background: #fff;
+  text-decoration: none;
+  color: #000;
+  font-weight: bold;
+  font-family: sans-serif;
+  overflow: hidden;
 }
 </style>
