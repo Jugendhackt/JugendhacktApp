0a35b4c749a412f47507c6abe88abacd86a7399d
marvinborner <develop@marvinborner.de>
2019-08-31 11:27:49 +0200

    Added doc

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const api = express.Router();
-api.use("/github", (_, req) => github.get(data => req.send(data)));
+const apiUrls = {
+	"GitHub": "/api/github",
+	"HackDash": "/api/HackDash",
+	"Twitter": "/api/Twitter",
+	"Zulip": "/api/Zulip"
+}
+
+api.use("/github", (_, res) => github.get(data => res.send(data)));
+api.use("*", (_, res) => res.send(apiUrls));
 app.use("/api", api);
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
