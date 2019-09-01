65b3a44d2d9e2a792c7116df68319e9f9866fd61
marvinborner <develop@marvinborner.de>
2019-09-01 11:39:27 +0200

    Added board description

--- public/js/components/board.vue
+++ public/js/components/board.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="card">
     <h3>{{ board.title }}</h3>
-    
+    <p>{{ boardData.description }}</p>
   </div>
 </template>
 
 <script>
 module.exports = {
-  props: ['board']
+  props: ['board'],
+  data: function(){
+    return {
+      boardData: {},
+    }
+  },
+  methods: {
+    fetch(){
+      this.loading = true;
+      let xhr = new XMLHttpRequest();
+      xhr.addEventListener("load", () => {
+        console.log(xhr.response);
+        this.loading = false;
+        this.boardData = xhr.response;
+      });
+      xhr.open("GET", "/api/hackdash/board/" + this.board.domain);
+      xhr.responseType = "json";
+      xhr.send();
+    }
+  },
+  beforeMount() {
+    this.fetch();
+  },
+  watch: {
+    board: function() {
+      this.boardData.description = "Loading...";
+      this.fetch();
+    }
+  },
 }
 </script>
