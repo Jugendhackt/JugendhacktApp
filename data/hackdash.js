d145232c8707cf5e2ee303568c48b2165507b347
marvinborner <develop@marvinborner.de>
2019-09-01 10:18:12 +0200

    Improved api

--- data/hackdash.js
+++ data/hackdash.js
@@ -4,12 +4,12 @@ const request = require("./request.js");
 module.exports.listBoards = async () => {
 	const boards = await request.start("https://hackdash.org/api/v2/profiles/557323e279ef5d384ac04aeb");
 	let dashboards = boards["dashboards"];
-	dashboards = dashboards.filter(elem => elem["showcase"].length > 0);
+	dashboards = dashboards.filter(elem => elem.domain !== "testes");
 	return dashboards;
 }
 
 module.exports.getBoardInfo = async board => {
-	const data = await request.start("https://hackdash.org/api/v2/dashboards/" + board);
+	const data = await request.start("https://hackdash.org/api/v2/" + board + "/projects");
 	return data;
 }
 
