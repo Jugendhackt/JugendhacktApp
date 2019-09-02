f83bd46554405f8a58ebcbc67639958404f9b3ab
Marvin Borner <marvin-borner@live.com>
2019-09-03 00:32:43 +0200

    Added feed icon and caching improvements

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,56 +1,71 @@
-const name = 'Jugendhackt';
+const name = "Jugendhackt";
 const filesToCache = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/js/script.js',
-    '/js/vue.js',
-    '/js/vue-router.js',
-    '/js/vue-loader.js',
-    '/views/events.vue',
-    '/views/login.vue',
-    '/js/components/navigation.vue',
-    '/service-worker.js',
-    '/api/',
-    '/api/events',
-    '/api/twitter',
-    '/api/hackdash',
-    '/api/events/',
-    '/api/twitter/',
-    '/api/hackdash/',
-    '/assets/icons/calendar.svg',
-    '/assets/icons/key.svg',
-    '/assets/icons/clipboard.svg',
-    '/assets/icons/arrow-down.svg',
-    '/assets/icons/briefcase.svg',
-    '/assets/icons/list.svg',
-    '/assets/icons/badges/SuperAlpaka.png'
+    "/",
+    "/jugendhackt.webmanifest",
+    "/index.html",
+    "/service-worker.js",
+
+    "/css/style.css",
+
+    "/js/script.js",
+    "/js/vue.js",
+    "/js/vue-loader.js",
+    "/js/vue-router.js",
+
+    "/views/events.vue",
+    "/views/feed.vue",
+    "/views/hackdash.vue",
+    "/views/login.vue",
+    "/views/lostitems.vue",
+    "/views/packinglist.vue",
+
+    "/api/",
+    "/api/events",
+    "/api/twitter",
+    "/api/hackdash",
+    "/api/events",
+    "/api/twitter",
+    "/api/hackdash",
+    "/api/zulip",
+
+    "/assets/icons/arrow-down.svg",
+    "/assets/icons/briefcase.svg",
+    "/assets/icons/calendar.svg",
+    "/assets/icons/clipboard.svg",
+    "/assets/icons/feed.svg",
+    "/assets/icons/key.svg",
+    "/assets/icons/list.svg",
+    "/assets/icons/badges/SuperAlpaka.png",
+    "/assets/images/default-background.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
     console.log("installing");
     event.waitUntil(
         caches.open(name).then(cache => {
-            return cache.addAll(filesToCache);
+            console.log("Added files to cache!");
+            return cache.addAll(filesToCache)
         })
     );
 });
 
-self.addEventListener('activate', event => {
-    console.log("activating");
-    event.waitUntil(
-        caches.keys().then(cachedFiles => {
-            return Promise.all(cachedFiles.map(cacheFile => {
-                if (cacheFile !== name) {
-                    console.log('Removing Cached Files from Cache - ', cacheFile);
-                    return caches.delete(cacheFile);
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.open(name).then(cache => {
+            return cache.match(event.request.url).then(response => {
+                if (response) {
+                    console.log("Fetching files from cache!");
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    return cache.match("/index.html");
                 }
-            }));
-        })
-    );
-});
 
-self.addEventListener('fetch', event => {
-    console.log("fetching");
-    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+                return fetch(event.request).then(response => {
+                    cache.put(event.request, response.clone());
+                    console.log("Fetching files from internet!");
+                    return response;
+                });
+            });
+        })
+    )
 });
