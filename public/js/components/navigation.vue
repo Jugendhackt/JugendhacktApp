483adc8d5c9566deaeb3923c5db545a48e1d3d9e
Marvin Borner <marvin-borner@live.com>
2019-09-02 17:53:19 +0200

    Added images

--- public/js/components/navigation.vue
+++ public/js/components/navigation.vue
@@ -1,14 +1,14 @@
 <template>
-<nav class="navigation">
-  <div class="branding">
-    Jugendhackt App
-  </div>
-  <div class="nav">
-    <router-link to="/"><img class="icon" src="assets/icons/calendar.svg" alt="Events"></router-link>
-    <router-link to="/hackdash"><img class="icon" src="assets/icons/clipboard.svg" alt="Hackdash"></router-link>
-    <router-link to="/packingList"><img class="icon" src="assets/icons/list.svg" alt="Packing List"></router-link>
-    <router-link to="/lostitems"><img class="icon" src="assets/icons/briefcase.svg" alt="Lost and Found"></router-link>
-    <router-link to="/login"><img class="icon" src="assets/icons/key.svg" alt="Login"></router-link>
-  </div>
-</nav>
+    <nav class="navigation">
+        <div class="branding">
+            Jugendhackt App
+        </div>
+        <div class="nav">
+            <router-link to="/"><img class="icon" src="assets/icons/calendar.svg" alt="Events"></router-link>
+            <router-link to="/hackdash"><img class="icon" src="assets/icons/clipboard.svg" alt="Hackdash"></router-link>
+            <router-link to="/packingList"><img class="icon" src="assets/icons/list.svg" alt="Packing List"></router-link>
+            <router-link to="/lostitems"><img class="icon" src="assets/icons/briefcase.svg" alt="Lost and Found"></router-link>
+            <router-link to="/login"><img class="icon" src="assets/icons/key.svg" alt="Login"></router-link>
+        </div>
+    </nav>
 </template>
