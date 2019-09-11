151b881a2a3a221ba8953a5be16eee072247f6b3
Marvin Borner <marvin-borner@live.com>
2019-09-11 18:28:29 +0200

    Fixed arbitrary directory constants

--- badgr.sh
+++ badgr.sh
@@ -4,7 +4,8 @@
 #     $ crontab -e
 #     * */20 * * * $USER /path/to/cron.sh >/dev/null 2>&1 # Updates every 20 hours
 
-EMAIL=$(cat ./.env | grep BadgrMail | cut -f2 -d'=')
-PASS=$(cat ./.env | grep BadgrPass | cut -f2 -d'=')
+PROJECT_DIR="${HOME}/jugendhacktapp/" # Sorry for constant directory
+EMAIL=$(cat "${PROJECT_DIR}/.env" | grep BadgrMail | cut -f2 -d'=')
+PASS=$(cat "${PROJECT_DIR}/.env" | grep BadgrPass | cut -f2 -d'=')
 ACCESS=$(curl -X POST 'https://api.badgr.io/o/token' -d "username=${EMAIL}&password=${PASS}" | jq .access_token | sed 's/\"//g')
-sed -i "/^BadgrAccess=/s/=.*/=$ACCESS/" .env
+sed -i "/^BadgrAccess=/s/=.*/=$ACCESS/" "${PROJECT_DIR}/.env"
