cc657f1cd17880f229818346ef11e1ba80dffebd
BuildTools <unconfigured@null.spigotmc.org>
2019-10-05 14:43:16 +0200

    Editing a 404 page

--- public/views/404.vue
+++ public/views/404.vue
@@ -2,6 +2,6 @@
   <div class="card">
       <h2 class="name">404 Error</h2>
       <sup> Page not found! <sup>
-      <i> We searched and searched for your request but we can't find it! <i>
+      
 </div>
 </template>
