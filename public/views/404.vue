3c36c8d287499fc9990585d2821d85c7073ee303
SirLennox <SirLennox@gmx.de>
2019-10-05 15:20:23 +0200

    Added something to 404 page

--- public/views/404.vue
+++ public/views/404.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="card">
       <h2 class="name">404 Error</h2>
-      <i> Page not found! <i>
-      
+      <i>Page not found!<i>
+      <br>
+      <i>Go to <u><b>https://www.alpaca.ml/<u><b>
 
 </div>
 </template>
