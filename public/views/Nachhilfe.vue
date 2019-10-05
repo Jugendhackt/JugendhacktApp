20345ffc199c06d158a0697b156088a7037ee4c4
SirLennox <SirLennox@gmx.de>
2019-10-05 16:40:43 +0200

    Nachhilfe updated

--- public/views/Nachhilfe.vue
+++ public/views/Nachhilfe.vue
@@ -2,14 +2,16 @@
   <div class="card">
       <h2 class="name">Nachhilfe</h2>
 
-      <label for="Message">Message:</label>
+
+
+    <label for="Message">Message:</label>
   </div>
 
 </template>
 <style>
 label{
   margin-top: 8em;
-  
+
 }
 
 
