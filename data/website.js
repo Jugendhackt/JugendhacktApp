ebf3ee464f1a94c3bd007a8c75ccab036faf7967
Marvin Borner <develop@marvinborner.de>
2019-12-16 21:49:52 +0100

    Added new website support!

--- data/website.js
+++ data/website.js
@@ -5,21 +5,26 @@ const {JSDOM} = jsdom;
 module.exports.getEvents = async () => {
     const html = await request.start("https://jugendhackt.org/events/", false);
     const dom = new JSDOM(html);
-    const events = dom.window.document.querySelectorAll(".teaser-item-events");
+
+    const events = dom.window.document.querySelectorAll(".c-events-list ul");
     const eventsArr = [];
     for (event of events) {
         const object = {};
-        object["img"] = event.querySelector("img.attachment-event-teaser").src;
-        const dateLocation = event.querySelectorAll("h2");
-        object["date"] = dateLocation[0].textContent;
-        object["location"] = dateLocation[1].textContent;
-        const buttons = event.querySelectorAll(".button");
-        if (buttons[0].textContent === "Anmelden") {
-            object["anmelden"] = buttons[0].href;
-            object["information"] = buttons[1].href;
-        } else {
-            object["anmelden"] = "expired";
-            object["information"] = buttons[0].href;
+        try {
+            object["img"] = event.querySelector(".events-list-item picture source").srcset.slice(0, -12) + ".jpg"; // high quality!
+            object["location"] = event.querySelector(".events-list-item .events-list-body .events-list-title a").textContent;
+            const buttons = event.querySelectorAll(".events-list-actions a");
+
+            if (buttons[1].textContent === " Anmelden") { // Thx for the space @JH!
+                object["anmelden"] = buttons[1].href;
+                object["information"] = buttons[0].href;
+            } else {
+                object["anmelden"] = "expired";
+                object["information"] = buttons[0].href;
+            }
+            object["date"] = event.getElementsByTagName("time")[0].textContent; // at the end because this seems to fail quite often?!
+        } catch (e) {
+            // Jugend hackt has a new website again!
         }
         eventsArr.push(object);
     }
