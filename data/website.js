854afb38f58d8b565ab162f2c2433fcbdd669899
Marvin Borner <develop@marvinborner.de>
2019-12-16 22:01:39 +0100

    Woopsidoopsi

--- data/website.js
+++ data/website.js
@@ -6,23 +6,24 @@ module.exports.getEvents = async () => {
     const html = await request.start("https://jugendhackt.org/events/", false);
     const dom = new JSDOM(html);
 
-    const events = dom.window.document.querySelectorAll(".c-events-list ul");
+    const correctSection = dom.window.document.querySelectorAll("section.c-events-list")[0];
+    const events = correctSection.querySelectorAll(".c-events-list ul li");
     const eventsArr = [];
     for (event of events) {
         const object = {};
         try {
             object["img"] = event.querySelector(".events-list-item picture source").srcset.slice(0, -12) + ".jpg"; // high quality!
             object["location"] = event.querySelector(".events-list-item .events-list-body .events-list-title a").textContent;
-            const buttons = event.querySelectorAll(".events-list-actions a");
+            object["date"] = event.getElementsByTagName("time")[0].textContent;
 
-            if (buttons[1].textContent === " Anmelden") { // Thx for the space @JH!
+            const buttons = event.querySelectorAll(".events-list-actions a");
+            if (buttons.length > 1 && buttons[1].textContent === " Anmelden") { // Thx for the space @JH!
                 object["anmelden"] = buttons[1].href;
                 object["information"] = buttons[0].href;
             } else {
                 object["anmelden"] = "expired";
                 object["information"] = buttons[0].href;
             }
-            object["date"] = event.getElementsByTagName("time")[0].textContent; // at the end because this seems to fail quite often?!
         } catch (e) {
             // Jugend hackt has a new website again!
         }
